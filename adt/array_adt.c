6430e68273e27d806b5e61e694dbdcb9cbc19254
AustinMay1 <austin.may@protonmail.com>
2023-07-27 20:02:45 -0400

    more adt functions

--- adt/array_adt.c
+++ adt/array_adt.c
@@ -2,30 +2,23 @@
 #include "array_adt.h"
 
 int main(void) {
-    struct Array arr;
-    int n, i;
-    printf("Enter array size");
-    scanf("%d", &arr.size);
-
-    arr.A=(int *)malloc(arr.size * sizeof(int));
-    arr.length = 0;
-
-    printf("Enter # of elements to add");
-    scanf("%d", &n);
-
-    printf("Enter all elements\n");
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr.A[i]);
-    }
-    arr.length = n;
-
+    struct Array arr = {{2,4,6,8,10}, 10, 5};
+    append(&arr, 12);
+    insert(&arr, 3, 7);
     display(arr);
-    printf("\n");
-    append(&arr, 100);
-    insert(&arr, 1, 22);
+
+    printf("Element found at index: %d\n", binary_search(arr, 4));
+    printf("E found at index 3: %d\n", get_element(arr, 3));
+    set_element(&arr, 0, 1);
+    printf("Set element at index 0 to 1\n");
     display(arr);
-    printf("%d\n", arr_remove(&arr, 1));
+    printf("Max: %d\n", max(arr));
+    printf("Min: %d\n", min(arr));
+    printf("Sum: %d\n", sum(arr));
+    printf("Avg: %.2f\n", avg(arr));
+
+    printf("Reversed array: ");
+    reverse_swap(&arr);
     display(arr);
-    printf("%d", linear_search(arr, 3));    
     return 0;
 }
--- adt/array_adt.h
+++ adt/array_adt.h
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Array {
-    int *A;
+    int A[10];
     int size;
     int length;
 };
 
 void display(struct Array arr) {
     int i;
-
+    printf("{");
     for(i = 0; i < arr.length; i++) {
-        printf("%d ", arr.A[i]);
+        printf(" %d ", arr.A[i]);
     }
+    printf("}\n");
 }
 
 void append(struct Array *arr, int x) {
@@ -47,10 +49,128 @@ int arr_remove(struct Array *arr, int index) {
     return 0;
 }
 
-int linear_search(struct Array arr, int x) {
-    for(int i = 0; i < arr.length; i++) {
-        if(arr.A[i] == x) {
+void swap(int *x, int *y) {
+    int t;
+
+    t = *x;
+    *x = *y;
+    *y = t;
+} // transposition for improving linear search.
+
+int linear_search(struct Array *arr, int key) {
+    for(int i = 0; i < arr->length; i++) {
+        if(arr->A[i] == key) {
+            swap(&arr->A[i], &arr->A[0]); // move found element to head for O(1) access on future searches.
             return i;
         }
+    }
+    return -1;
+}
+
+int binary_search(struct Array arr, int key) {
+    int low, mid, hi;
+    low = 0;
+    hi = arr.length - 1;
+
+    while(low <= hi) {
+        mid = (low + hi) / 2;
+
+        if (key == arr.A[mid]) {
+            return mid;
+        } else if (key < arr.A[mid]) {
+            hi = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+} // array must be sorted for binary search to work
+
+int rec_binary_search(int arr[], int l, int h, int key) {
+    int mid;
+
+    if(l <= h) {
+        mid = (l + h) / 2;
+        if(key == arr[mid]) {
+            return mid;
+        } else if (key < arr[mid]) {
+            return rec_binary_search(arr, l, mid - 1, key);
+        } else {
+            return rec_binary_search(arr, mid + 1, h, key);
+        }
+    }
+    return -1;
+}
+
+int get_element(struct Array arr, int index) {
+    if(index >= 0 && index < arr.length) {
+        return arr.A[index];
+    }
+    return -1;
+}
+
+void set_element(struct Array *arr, int index, int x) {
+    if(index >= 0 && index < arr->length) {
+        arr->A[index] = x;
+    }
+}
+
+int max(struct Array arr) {
+    int max = arr.A[0];
+
+    for(int i = 1; i < arr.length; i++) {
+        if(arr.A[i] > max) {
+            max = arr.A[i];
+        }
+    }
+     return max;
+}
+
+int min(struct Array arr) {
+    int min = arr.A[0];
+
+    for(int i = 1; i < arr.length; i++) {
+        if(arr.A[i] < min) {
+            min = arr.A[i];
+        }
+    }
+
+    return min;
+}
+
+int sum(struct Array arr) {
+    int s = 0;
+
+    for(int i = 0; i < arr.length; i++) {
+        s += arr.A[i];
+    }
+
+    return s;
+}
+
+float avg(struct Array arr) {
+    return (float) sum(arr) / arr.length;
+}
+
+void reverse(struct Array *arr) {
+    int *b;
+    int i,j;
+
+    b=(int *)malloc(arr->length * sizeof(int));
+
+    for(i = arr->length - 1, j = 0; i >= 0; i--, j++) {
+        b[j] = arr->A[i];
+    }
+
+    for(i = 0; i > arr->length; i++) {
+        arr->A[i] = b[i];
+    }
+}
+
+void reverse_swap(struct Array *arr) {
+    int i, j;
+
+    for(i = 0, j = arr->length - 1; i < j; i++, j--) {
+        swap(&arr->A[i], &arr->A[j]);
     }
 }
